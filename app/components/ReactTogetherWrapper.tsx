6312a7b599d924848a4047ba16581122eadda997
sifu <sifu213@gmail.com>
2025-07-18 00:08:26 +0200

    conf appid

--- app/components/ReactTogetherWrapper.tsx
+++ app/components/ReactTogetherWrapper.tsx
@@ -12,7 +12,7 @@ export default function ReactTogetherWrapper({
   const sessionParams = {
     appId:
       process.env.NEXT_PUBLIC_DEFAULT_APP_ID ||
-      "monad-together",
+      "io.multisynq.monad-together",
     apiKey: process.env.NEXT_PUBLIC_MULTISYNQ_API_KEY || "20Ovt2JGkI7CdZQZnZDBLr9sq7x7KwWxEOYuBSrK1C",
     name: process.env.NEXT_PUBLIC_DEFAULT_SESSION_NAME || "default-session",
     password: process.env.NEXT_PUBLIC_DEFAULT_SESSION_PASSWORD || "demo123",
